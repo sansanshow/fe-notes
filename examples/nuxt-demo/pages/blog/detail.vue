db9a754dc82a28c97c2f20e9e32b4d0039d3a5ff
maweiqiang <maweiqiang@f-road.com.cn>
2018-06-29 18:31:45 +0800

    add nuxt-demo

---
+++ examples/nuxt-demo/pages/blog/detail.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3> detail</h3>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+h3{
+    color: red;
+}
+</style>
+
+
---
+++ examples/nuxt-demo/pages/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div>
+      <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
+        <mt-button icon="back">返回</mt-button>
+        <mt-button icon="more" slot="right"></mt-button>
+      </mt-header>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+</style>
