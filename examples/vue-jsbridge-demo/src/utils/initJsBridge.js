abed78fce8428a2c1937cc065a65a24ffa796148
kovii <sansan_show@126.com>
2019-03-06 17:06:28 +0800

    demo

---
+++ examples/vue-jsbridge-demo/src/utils/initJsBridge.js
@@ -0,0 +1,27 @@
+window.JSBridge = {};
+
+function callJS (action, params, whoAmI) {
+  params = JSON.parse(JSON.stringify(params));
+  switch (action) {
+    case 'showSkill':
+      const category = params.category;
+      JSBridge.showSkill(category);
+      break;
+    case 'showSkillDetail':
+      const id = params.id;
+      JSBridge.showSkillDetail(id);
+      break;
+    case 'otherAction':
+      // some code
+      break;
+    default:
+      alert(1);
+      break;
+
+  }
+}
+
+// JS暴露给应用的通用接口
+const SpkJSBridge = {}
+// 全部接口
+window.JSBridge.callJS = callJS
---
+++ examples/vue-jsbridge-demo/src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <p>1111</p>
+  </section>
+</template>
+<script>
+import '@/utils/initJsBridge'
+export default {
+  
+}
+</script>
+<style lang="less">
+
+</style>
+
