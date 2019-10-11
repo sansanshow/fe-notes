b04f301176cf806e6cc9f4fac73aefac2e106e83
kovii <sansan_show@126.com>
2019-10-11 10:10:49 +0800

    pwa & vue-pwa

---
+++ examples/pwa-test/public/index.js
@@ -0,0 +1,20 @@
+var version = 'v3';
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/server-worker.js').then(reg => {
+        console.log('service worker registed!');
+        console.log('sw_version: ', localStorage.getItem('sw_version'), ', version: ', version);
+        reg.update();
+        if (localStorage.getItem('sw_version') !== version) {
+            console.log('reg:--update')
+            reg.update().then(function () {
+                localStorage.setItem('sw_version', version)
+            })
+        }
+    }).catch(err => {
+        console.log('Opooos, something wrong happend!');
+    })
+}
+
+window.onload = function() {
+    document.body.append('PWA!')
+}
