76a157cf8154d696000833456812d05043a57c38
kovii <sansan_show@126.com>
2019-12-15 19:49:11 +0800

    some demo

--- examples/react/react-demo/src/App.js
+++ examples/react/react-demo/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {HashRouter, Route, Switch} from 'react-router-dom'
-
+import { BrowserRouter } from 'react-router-dom'
 import './App.scss';
 import './assets/iconfont/iconfont.css';
 import './styles/common/common.scss';
@@ -11,12 +11,12 @@ class App extends Component {
   render() {
     
     return (
-      <HashRouter>
+      <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
         </Switch>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
--- examples/react/react-demo/src/components/form/fx-input/index.js
+++ examples/react/react-demo/src/components/form/fx-input/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
-
+import './styles.scss';
 class FxInput extends React.Component {
-  
+  constructor() {
+    this.onChange = this.props.onChange
+  }
+  render() {
+    return (
+        <div className="inputBox mobile">
+            <span className="label">{this.props.label}</span>
+            <input type="tel" placeholder={this.props.placeholder || "请输入您的手机号"} name="mobile" className="input mobileInput" autoComplete="false"
+                value={this.props.value} onChange={this.onChange} maxLength={this.props.maxLength}/>
+                {/* {this.state.loginForm.mobile ?
+                    <i className="clear-btn iconfont icon-yuanxingguanbianniu" onClick={ this.clearMobileHandle.bind(this) }></i>
+                    : ''
+                } */}
+        </div>
+    )
+  }
 }
 
 export default FxInput;
